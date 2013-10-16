8715e9bc1e806d61d5cfcf75bf2a78ee05fdd2ee
Anastasis Andronidis <anastasis90@yahoo.gr>
2013-10-16 12:27:25 +0300

    changed script to reflect the new API of file handling

--- helpers/ar-range.sh
+++ helpers/ar-range.sh
@@ -3,6 +3,8 @@
 currentdate=$1
 loopenddate=$(/bin/date --date "$2 1 day" +%Y-%m-%d)
 
+cd /var/lib/ar-sync/
+
 until [ "$currentdate" == "$loopenddate" ]; do
 
   RUN_DATE=$currentdate
@@ -14,27 +16,31 @@ until [ "$currentdate" == "$loopenddate" ]; do
 
   ### prepare poems
   echo "Prepare poems for $RUN_DATE"
-  grep ROC_CRITICAL /var/lib/ar-sync/poem_sync_$RUN_DATE_UNDER.out > /var/lib/ar-sync/poem_sync_$RUN_DATE_UNDER.out.roc_critical
+  grep ROC_CRITICAL poem_sync_$RUN_DATE_UNDER.out | cut -d $(echo -e '\x01') --output-delimiter=$(echo -e '\x01') -f "3 4 5 6"| grep "ch.cern.sam.ROC_CRITICAL" | sort -u | awk 'BEGIN {ORS="|"; RS="\n"} {print $0}' | gzip -c | base64 | awk 'BEGIN {ORS=""} {print $0}' > poem_sync_$RUN_DATE_UNDER.out.clean
 
-  ### run topology and upload the topology with name: topology.txt (remove previous topology.txt)
+  ### prepare topology
   echo "Prepare topology for $RUN_DATE"
-  hadoop fs -rm -skipTrash topology_$RUN_DATE.txt
-  cat /var/lib/ar-sync/sites_$RUN_DATE_UNDER.out | sort -u > /var/lib/ar-sync/sites_$RUN_DATE_UNDER.out.clean
-  cat /var/lib/ar-sync/sites_$RUN_DATE_UNDER.out.clean | sed 's/\x01/ /g' | grep " SRM " | sed 's/ SRM / SRMv2 /g' | sed 's/ /\x01/g' >> /var/lib/ar-sync/sites_$RUN_DATE_UNDER.out.clean
-  hadoop fs -put /var/lib/ar-sync/sites_$RUN_DATE_UNDER.out.clean topology_$RUN_DATE.txt
-  rm -f /var/lib/ar-sync/sites_$RUN_DATE_UNDER.out.clean
+  cat sites_$RUN_DATE_UNDER.out | sort -u > sites_$RUN_DATE_UNDER.out.clean
+  cat sites_$RUN_DATE_UNDER.out.clean | sed 's/\x01/ /g' | grep " SRM " | sed 's/ SRM / SRMv2 /g' | sed 's/ /\x01/g' >> sites_$RUN_DATE_UNDER.out.clean
+  cat sites_$RUN_DATE_UNDER.out.clean | awk 'BEGIN {ORS="|"; RS="\r\n"} {print $0}' | gzip -c | base64 | awk 'BEGIN {ORS=""} {print $0}'` > sites_$RUN_DATE_UNDER.zip
+  rm -f sites_$RUN_DATE_UNDER.out.clean
+  split -b 30092 sites_$RUN_DATE_UNDER.zip sites_$RUN_DATE_UNDER
+  
 
   ### prepare downtimes
   echo "Prepare downtimes for $RUN_DATE"
-  # /usr/libexec/ar-sync/downtime-sync -d $RUN_DATE
-  cat /var/lib/ar-sync/downtimes_$RUN_DATE.out | sed 's/\x01/ /g' | grep " SRM " | sed 's/ SRM / SRMv2 /g' | sed 's/ /\x01/g' > /var/lib/ar-sync/downtimes_cache_$RUN_DATE.out
-  cat /var/lib/ar-sync/downtimes_$RUN_DATE.out >> /var/lib/ar-sync/downtimes_cache_$RUN_DATE.out
+  /usr/libexec/ar-sync/downtime-sync -d $RUN_DATE
+  cat downtimes_$RUN_DATE.out | sed 's/\x01/ /g' | grep " SRM " | sed 's/ SRM / SRMv2 /g' | sed 's/ /\x01/g' > downtimes_cache_$RUN_DATE.out
+  cat downtimes_$RUN_DATE.out >> downtimes_cache_$RUN_DATE.out
+  cat downtimes_cache_$RUN_DATE.out | awk 'BEGIN {ORS="|"; RS="\r\n"} {print $0}' | gzip -c | base64 | awk 'BEGIN {ORS=""} {print $0}' > downtimes_$RUN_DATE.zip
+  rm -f downtimes_cache_$RUN_DATE.out
 
   ### run calculator.pig
-  pig -useHCatalog -param in_date=$RUN_DATE -param downtimes_file=/var/lib/ar-sync/downtimes_cache_$RUN_DATE.out -param poem_file=/var/lib/ar-sync/poem_sync_$RUN_DATE_UNDER.out.roc_critical -f /usr/libexec/ar-compute/pig/calculator.pig
+  pig -useHCatalog -param in_date=$RUN_DATE -param downtimes_file=downtimes_$RUN_DATE.zip -param poem_file=poem_sync_$RUN_DATE_UNDER.out.clean -param topology_file1=sites_$RUN_DATE_UNDERaa -param topology_file2=sites_$RUN_DATE_UNDERab -param topology_file3=sites_$RUN_DATE_UNDERac -f /usr/libexec/ar-compute/pig/calculator.pig
 
-  rm -f /var/lib/ar-sync/poem_sync_$RUN_DATE_UNDER.out.roc_critical
-  rm -f /var/lib/ar-sync/downtimes_cache_$RUN_DATE.out
+  rm -f poem_sync_$RUN_DATE_UNDER.out.clean
+  rm -f downtimes_$RUN_DATE.zip
+  rm -f sites_$RUN_DATE_UNDERaa sites_$RUN_DATE_UNDERab sites_$RUN_DATE_UNDERac
   
   currentdate=$(/bin/date --date "$currentdate 1 day" +%Y-%m-%d)
   
@@ -43,5 +49,3 @@ done
 echo "waiting for hadoop"
 
 wait
-
-hadoop fs -rm -skipTrash topology_*
