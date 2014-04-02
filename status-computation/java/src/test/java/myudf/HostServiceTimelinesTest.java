355a4bbef826866108a15f8a4443e74573a6a9b3
Konstantinos Kagkelidis <kaggis@gmail.com>
2014-04-02 09:17:05 +0300

    Add unit test structure for MyUdf. Add tests for class utils.Utils

---
+++ status-computation/java/src/test/java/myudf/HostServiceTimelinesTest.java
@@ -0,0 +1,30 @@
+package myudf;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class HostServiceTimelinesTest {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Ignore
+	public void testExecTuple() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ status-computation/java/src/test/java/utils/ExteralResourcesTest.java
@@ -0,0 +1,55 @@
+package utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class ExteralResourcesTest {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Ignore
+	public void testGetDowntimes() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	public void testInitPOEMs() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	public void testInitWeights() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	public void testInitAPs() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	public void testGetSFtoAvailabilityProfileNames() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	public void testGetRecalculationRequests() {
+		fail("Not yet implemented");
+	}
+
+}
