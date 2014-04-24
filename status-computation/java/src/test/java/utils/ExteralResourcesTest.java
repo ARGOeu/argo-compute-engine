ef112359a6c9aded43f50141b7fb19e121edb8b0
Konstantinos Kagkelidis <kaggis@gmail.com>
2014-04-25 02:11:10 +0300

    Add Embedded Mongo Server contained in unit tests

--- status-computation/java/src/test/java/utils/ExteralResourcesTest.java
+++ status-computation/java/src/test/java/utils/ExteralResourcesTest.java
@@ -8,12 +8,20 @@ import static org.junit.Assert.*;
 import org.apache.commons.io.IOUtils;
 import org.apache.pig.data.DataBag;
 
+
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.ConsoleHandler;
+
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 import org.json.JSONException;
 import org.junit.After;
@@ -24,11 +32,30 @@ import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 
 import com.google.gson.Gson;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.util.JSON;
+
+import de.flapdoodle.embed.mongo.Command;
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodProcess;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.config.RuntimeConfigBuilder;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.config.IRuntimeConfig;
+import de.flapdoodle.embed.process.config.io.ProcessOutput;
+import de.flapdoodle.embed.process.runtime.Network;
 
 public class ExteralResourcesTest {
 
-	public Gson gson; 
-	
+	private Gson gson; 
+	private MongodStarter starter = MongodStarter.getDefaultInstance();
+	private MongodExecutable mongodExe;
+	private MongodProcess mongod;
 	
 	
 	@AfterClass
@@ -38,10 +65,66 @@ public class ExteralResourcesTest {
 	@Before
 	public void setUp() throws Exception {
 		gson = new Gson();
+		
+		Logger globalLogger = Logger.getLogger("global");
+		Handler[] handlers = globalLogger.getHandlers();
+		for(Handler handler : handlers) {
+		    globalLogger.removeHandler(handler);
+		}
+		
+		
+
+	    IRuntimeConfig runtimeConfig = new RuntimeConfigBuilder()
+	        .defaultsWithLogger(Command.MongoD, globalLogger)
+	        .processOutput(ProcessOutput.getDefaultInstanceSilent())
+	        .build();
+
+	    
+
+		starter = MongodStarter.getInstance(runtimeConfig);
+		
+		
+		
+		//Initialize Mongo Server
+		mongodExe = starter.prepare(new MongodConfigBuilder()
+        .version(Version.Main.PRODUCTION)
+        .net(new Net(12345, Network.localhostIsIPv6()))
+        .build());
+
+		LogManager.getLogManager().reset();
+		mongod = mongodExe.start();
+		
+		
+		
+		
+		// Add Data
+		assertNotNull("Test file missing", getClass().getResource("/mongodata/mongo.aps.json"));
+		assertNotNull("Test file missing", getClass().getResource("/mongodata/mongo.recalc.json"));
+		
+		String  mongo_aps_data = IOUtils.toString(this.getClass().getResourceAsStream("/mongodata/mongo.aps.json"),"UTF-8");
+		String  mongo_recalc_data = IOUtils.toString(this.getClass().getResourceAsStream("/mongodata/mongo.recalc.json"),"UTF-8");
+		
+		MongoClient mongoClient = new MongoClient("localhost", 12345);
+		
+		DB db = mongoClient.getDB( "AR" );
+	    DBCollection aps_col = db.getCollection("aps");
+	    DBCollection recalc_col = db.getCollection("recalculations");
+	    DBObject aps_data = (DBObject)JSON.parse(mongo_aps_data);
+	    DBObject recalc_data = (DBObject)JSON.parse(mongo_recalc_data);   
+	    aps_col.insert(aps_data);
+	    recalc_col.insert(recalc_data);
+	    
+	    
+		
+		
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		if (mongod != null) mongod.stop();
+		if (mongodExe != null) mongodExe.stop();
+		
+		
 	}
 
 	
@@ -54,8 +137,10 @@ public class ExteralResourcesTest {
 	   assertNotNull("Test file missing", getClass().getResource("/weights/weightsIn.txt"));
 	   assertNotNull("Test file missing", getClass().getResource("/weights/weightsOut.txt"));
 	   assertNotNull("Test file missing", getClass().getResource("/aps/apsOut.txt"));
-	   assertNotNull("Test file missing", getClass().getResource("/aps/sf2aps.txt"));
 	   assertNotNull("Test file missing", getClass().getResource("/recalc/recalcOut.txt"));
+	   assertNotNull("Test file missing", getClass().getResource("/aps/sf2aps.txt"));
+	   assertNotNull("Test file missing", getClass().getResource("/mongodata/mongo.aps.json"));
+	   assertNotNull("Test file missing", getClass().getResource("/mongodata/mongo.recalc.json"));
 	}
 	
 	
@@ -113,7 +198,7 @@ public class ExteralResourcesTest {
 	public void testInitAPs() throws FileNotFoundException, IOException, JSONException {
 		Map<String, Map<String, Integer>> result = new HashMap<String, Map<String, Integer>>();
 		String resultString = IOUtils.toString(this.getClass().getResourceAsStream("/aps/apsOut.txt"),"UTF-8");
-		result = ExternalResources.initAPs("localhost", 27017);
+		result = ExternalResources.initAPs("localhost", 12345);
 	
 		String r_to_json = gson.toJson(result); 
 		
@@ -134,7 +219,7 @@ public class ExteralResourcesTest {
 	@Test
 	public void testGetRecalculationRequests() throws UnknownHostException, IOException, JSONException {
 		Map<String, Map<String, Object>> result = new HashMap<String, Map<String, Object>>(10);
-		result = ExternalResources.getRecalculationRequests("localhost", 27017, 20131209, 288);
+		result = ExternalResources.getRecalculationRequests("localhost", 12345, 20131209, 288);
 		String resultString = IOUtils.toString(this.getClass().getResourceAsStream("/recalc/recalcOut.txt"),"UTF-8");
 		
 		
