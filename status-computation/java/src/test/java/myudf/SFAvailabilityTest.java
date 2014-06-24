19e7a8a59eab6fca80bb92a7c4ce1fe5156b09cc
Kostas Kagkelidis <kaggis@gmail.com>
2014-06-24 10:33:30 +0300

    Unit test case for service flavour availability fix

--- status-computation/java/src/test/java/myudf/SFAvailabilityTest.java
+++ status-computation/java/src/test/java/myudf/SFAvailabilityTest.java
@@ -2,11 +2,18 @@ package myudf;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.data.TupleFactory;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import TestIO.JsonToPig;
 
 public class SFAvailabilityTest {
 
@@ -22,9 +29,16 @@ public class SFAvailabilityTest {
 	public void tearDown() throws Exception {
 	}
 	
-	@Ignore
-	public void testExecTuple() {
-		fail("Not yet implemented");
+	@Test
+	public void testExecTuple() throws IOException {
+		String json_str = IOUtils.toString(this.getClass().getResourceAsStream("/sfa_data/sfa_input.json"),"UTF-8");
+		String expectedString = IOUtils.toString(this.getClass().getResourceAsStream("/sfa_data/sfa_output.txt"),"UTF-8");
+		TupleFactory tf = TupleFactory.getInstance();
+		Tuple cur = tf.newTuple();
+		Tuple tmp = JsonToPig.jsonToTuple(json_str);
+		SFAvailability sfa = new SFAvailability();
+		String resultString = sfa.exec(tmp).toString();
+		assertTrue(resultString.compareTo(expectedString) == 0);
 	}
 
 }
