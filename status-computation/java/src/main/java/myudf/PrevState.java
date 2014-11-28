c0f7a4ba2caf0ac84258f0704af29a8ee3333dee
Konstantinos Kagkelidis <kaggis@gmail.com>
2014-11-28 07:26:42 +0200

    Support compute on timeline groups. Add constructor with mongodb config. Append site information

--- status-computation/java/src/main/java/myudf/PrevState.java
+++ status-computation/java/src/main/java/myudf/PrevState.java
@@ -1,112 +1,173 @@
 package myudf;
 
 import java.io.IOException;
+import java.net.UnknownHostException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.apache.pig.EvalFunc;
 import org.apache.pig.data.DataType;
 import org.apache.pig.data.Tuple;
+import org.apache.pig.data.DefaultDataBag;
+import org.apache.pig.data.TupleFactory;
 import org.apache.pig.impl.logicalLayer.FrontendException;
 import org.apache.pig.impl.logicalLayer.schema.Schema;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
 
 
 public class PrevState extends EvalFunc<Tuple> {
 	
-	private String endpoint_metric="";
-	private String prevState = "";
-	private static final String lastState ="LAST";
+	private static String mongo_host;
+	private static int mongo_port;
 	
+	public PrevState(String inp_mongo_host, String inp_mongo_port){
+		mongo_host = inp_mongo_host;
+		mongo_port = Integer.parseInt(inp_mongo_port);
+	}
 	
+	public PrevState() {
+    }
+	//private final TupleFactory mTupleFactory = TupleFactory.getInstance();
 	
+	private Map<String , String> sites = null;
+	// THIS EVAL FUNCTION ACCEPTS 
+	// (vo,vo_fqan,monitoring_box,roc,service_type,hostname,metric{(timestamp,status,summary,message)...})
 	
+	private void initSites() throws UnknownHostException{
+		// Initialize hashmap structure
+		sites = new HashMap<String, String>();
+		// Connect to the database and get site information
+		MongoClient mongoClient = new MongoClient(mongo_host, mongo_port);
+		DB db = mongoClient.getDB( "AR" );
+		DBCollection coll = db.getCollection("info_sites");
+		DBCursor cursor = coll.find();
+		DBObject item;
+		while(cursor.hasNext()) {
+		   item = cursor.next();
+		   sites.put((String)item.get("h"),(String)item.get("st"));
+		}
+		
+		
+	}
 	@Override
 	public Tuple exec(Tuple input) throws IOException {
 		
-		// Get endpoint-metric info
-		StringBuilder sb= new StringBuilder();
-		sb.append(input.get(5)).append(input.get(4)).append(input.get(3));
-				
-		// first iteration or change in metric
-		if ( (endpoint_metric.equals("")) ) {
-			input.append(lastState); //We've jumped to another metric
-			endpoint_metric = sb.toString(); //Set the endpoint_metric info
-		} else if (!(endpoint_metric.equals(sb.toString()))) {
-			input.append(lastState);
-			endpoint_metric = sb.toString();
-			
-		} else {
-			input.append(prevState);
 		
+		if (sites==null)
+		{
+			initSites();
 		}
 		
-		// Calculate integer of date and time
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-	    Date parsedDate = null;
-		try {
-			parsedDate = dateFormat.parse((String) input.get(0));
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		// parse input
+		//String vo = (String)input.get(0);
+		//String vo_fqan = (String)input.get(1);
+		//String monitoring_box = (String)input.get(2);
+		//String roc = (String)input.get(3);
+		//String service_type = (String)input.get(4);
+		String hostname = (String)input.get(5);
+		//String metric = (String)input.get(6);
+		
+		DefaultDataBag timeline = (DefaultDataBag)input.get(7);
+		Iterator<Tuple> myit = timeline.iterator();
+		Tuple item;
+		
+		String prevState = "LAST";
+		
+		for (int i=0;i<timeline.size();i++)
+		{
+			 item = myit.next();
+			 item.append(prevState);
+			 
+			 // Calculate integer of date and time
+			 SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z'");
+			 Date parsedDate = null;
+			 
+			 try {
+				 parsedDate = dateFormat.parse((String) item.get(0));
+			 } catch (ParseException e) {
+					e.printStackTrace();
+			 }
+			 Calendar cal = Calendar.getInstance();
+			 cal.setTime(parsedDate);
+			 int date_int = (cal.get(Calendar.YEAR) * 10000 ) + ((cal.get(Calendar.MONTH) + 1)*100) + (cal.get(Calendar.DAY_OF_MONTH));
+			 int time_int = (cal.get(Calendar.HOUR_OF_DAY) * 10000) + ((cal.get(Calendar.MINUTE)*100)) + (cal.get(Calendar.SECOND));
+			    
+			 item.append(date_int);
+			 item.append(time_int);
+			 
+			 prevState = (String) item.get(1);
 		}
-
-	    Calendar cal = Calendar.getInstance();
-	    cal.setTime(parsedDate);
-
-	    int the_hours = cal.get(Calendar.HOUR);
-	    int am = cal.get(Calendar.AM_PM);
-	    if (am>0) the_hours = the_hours + 12;
-	    
-	    int date_int = (cal.get(Calendar.YEAR) * 10000 ) + ((cal.get(Calendar.MONTH) + 1)*100) + (cal.get(Calendar.DAY_OF_MONTH));
-	    int time_int = (the_hours * 10000) + ((cal.get(Calendar.MINUTE)*100)) + (cal.get(Calendar.SECOND));
-	    
-	    input.append(date_int);
-	    input.append(time_int);
-		// set the new previous state before going on
-		prevState = (String) input.get(6);
-				
+		input.append((String)sites.get(hostname));
 		return input;
 		
 	}
 	
 	@Override
     public Schema outputSchema(Schema input) {
+        
+		Schema.FieldSchema vo = new Schema.FieldSchema("vo", DataType.CHARARRAY);
+		Schema.FieldSchema vo_fqan = new Schema.FieldSchema("vo_fqan", DataType.CHARARRAY);
+		Schema.FieldSchema monitoring_box = new Schema.FieldSchema("monitoring_box", DataType.CHARARRAY);
+		Schema.FieldSchema roc = new Schema.FieldSchema("roc", DataType.CHARARRAY);
+		Schema.FieldSchema hostname = new Schema.FieldSchema("hostname", DataType.CHARARRAY);
+		Schema.FieldSchema service_type = new Schema.FieldSchema("service_type", DataType.CHARARRAY);
+		Schema.FieldSchema metric = new Schema.FieldSchema("metric", DataType.CHARARRAY);
+		Schema.FieldSchema site = new Schema.FieldSchema("site", DataType.CHARARRAY);
+        
+      
+        
+        
         Schema.FieldSchema timestamp = new Schema.FieldSchema("timestamp", DataType.CHARARRAY);
-        Schema.FieldSchema roc = new Schema.FieldSchema("roc", DataType.CHARARRAY);
-        Schema.FieldSchema monitoring_box = new Schema.FieldSchema("monitoring_box", DataType.CHARARRAY);
-        Schema.FieldSchema metric = new Schema.FieldSchema("metric", DataType.CHARARRAY);
-        Schema.FieldSchema service_type = new Schema.FieldSchema("service_type", DataType.CHARARRAY);
-        Schema.FieldSchema hostname = new Schema.FieldSchema("hostname", DataType.CHARARRAY);
         Schema.FieldSchema status = new Schema.FieldSchema("status", DataType.CHARARRAY);
-        Schema.FieldSchema vo = new Schema.FieldSchema("vo", DataType.CHARARRAY);
-        Schema.FieldSchema vo_fqan = new Schema.FieldSchema("vo_fqan", DataType.CHARARRAY);
         Schema.FieldSchema summary = new Schema.FieldSchema("summary", DataType.CHARARRAY);
         Schema.FieldSchema message = new Schema.FieldSchema("message", DataType.CHARARRAY);
         Schema.FieldSchema prev_state = new Schema.FieldSchema("prev_state", DataType.CHARARRAY);
         Schema.FieldSchema date_int = new Schema.FieldSchema("date_int", DataType.INTEGER);
         Schema.FieldSchema time_int = new Schema.FieldSchema("time_int", DataType.INTEGER);
         
-       	Schema status_detail = new Schema();
+        Schema status_metric = new Schema();
+        Schema timeline = new Schema();
         
-        status_detail.add(timestamp);
-        status_detail.add(roc);
-        status_detail.add(monitoring_box);
-        status_detail.add(metric);
-        status_detail.add(service_type);
-        status_detail.add(hostname);
-        status_detail.add(status);
-        status_detail.add(vo);
-        status_detail.add(vo_fqan);
-        status_detail.add(summary);
-        status_detail.add(message);
-        status_detail.add(prev_state);
-        status_detail.add(date_int);
-        status_detail.add(time_int);
+        status_metric.add(vo);
+        status_metric.add(vo_fqan);
+        status_metric.add(monitoring_box);
+        status_metric.add(roc);
+        status_metric.add(hostname);
+        status_metric.add(service_type);
+        status_metric.add(metric);
+        
+        timeline.add(timestamp);
+        timeline.add(status);
+        timeline.add(summary);
+        timeline.add(message);
+        timeline.add(prev_state);
+        timeline.add(date_int);
+        timeline.add(time_int);
+        
+        Schema.FieldSchema timelines = null;
+        try {
+            timelines = new Schema.FieldSchema("timelines", timeline, DataType.BAG);
+        } catch (FrontendException ex) {
+            Logger.getLogger(AddTopology.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        status_metric.add(timelines);
+        status_metric.add(site);
         
         try {
-            return new Schema(new Schema.FieldSchema("status_detail", status_detail, DataType.TUPLE));
+            return new Schema(new Schema.FieldSchema("status_metric", status_metric, DataType.TUPLE));
         } catch (FrontendException ex) {
            
         }
