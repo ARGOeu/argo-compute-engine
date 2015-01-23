30b6662b4e053cd9fbc2c0549f7fcd33da5b3113
Konstantinos Kagkelidis <kaggis@gmail.com>
2015-01-23 16:02:15 +0200

    Minor Discrete Timeline Fixes

--- status-computation/java/src/main/java/ops/DTimeline.java
+++ status-computation/java/src/main/java/ops/DTimeline.java
@@ -11,78 +11,77 @@ public class DTimeline {
 	
 	
 	
-	private int start_state;	// state to define the beginning of the timeline  
-	private TreeMap<Integer,Integer> input_states; // input states with the timestamp converted to slots
+	private int startState;	// state to define the beginning of the timeline  
+	private TreeMap<Integer,Integer> inputStates; // input states with the timestamp converted to slots
 	
 	
-	private int s_period;       // sampling period measured in minutes
-	private int s_interval;   	// sampling interval measured in minutes;
+	private int sPeriod;       // sampling period measured in minutes
+	private int sInterval;   	// sampling interval measured in minutes;
 	
 	int[] samples;				// array of samples based on sampling frequency		
 	
 	DTimeline()	{
-		start_state = -1;
-		s_period = 1440;			// 1 day = 24 hours = 24 * 60 minutes = 1440 minutes
-		s_interval = 5;				// every 5 minutes;
-		samples = new int[1440/5]; //288 samples;
-		input_states = new TreeMap<Integer,Integer>();
+		this.startState = -1;
+		this.sPeriod = 1440;			// 1 day = 24 hours = 24 * 60 minutes = 1440 minutes
+		this.sInterval = 5;				// every 5 minutes;
+		this.samples = new int[1440/5]; //288 samples;
+		this.inputStates = new TreeMap<Integer,Integer>();
 		Arrays.fill(samples, -1);
 	}
 	
 
-	public void setSampling(int _period, int _interval) {
-		s_period = _period;
-		s_interval = _interval;
-		samples = new int[this.s_period/this.s_interval];
+	public void setSampling(int period, int interval) {
+		this.sPeriod = period;
+		this.sInterval = interval;
+		samples = new int[this.sPeriod/this.sInterval];
 	}
 	
 	public void clearSamples(){
-		samples = new int[this.s_period/this.s_interval];
+		samples = new int[this.sPeriod/this.sInterval];
 	}
 	
 	
 	public void clearTimestamps(){
-		start_state = -1;
-		input_states.clear();
+		startState = -1;
+		inputStates.clear();
 	}
 	
-	public void firstState(int _state)
+	public void firstState(int state)
 	{
-		this.start_state = _state;
+		this.startState = state;
 	}
 	
-	
-	public int timestampToSlot(String _timestamp) throws ParseException{
+	public int tsInt(String timestamp) throws ParseException{
 	
 		SimpleDateFormat w3c_date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-		Date parsedDate = w3c_date.parse(_timestamp);
+		Date parsedDate = w3c_date.parse(timestamp);
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(parsedDate);
 		
 		int total_minutes = (cal.get(Calendar.HOUR_OF_DAY) * 60) + cal.get(Calendar.MINUTE);
 		
-		return  (total_minutes/this.s_interval) -1; //Normalize for array indexing
+		return  (total_minutes/this.sInterval) -1; //Normalize for array indexing
 	}
 	
-	public void insertState(String _timestamp, int _state ) throws ParseException{
-		int slot = this.timestampToSlot(_timestamp);
-		this.input_states.put(slot,_state);
+	public void insert(String timestamp, int state ) throws ParseException{
+		int slot = this.tsInt(timestamp);
+		this.inputStates.put(slot,state);
 	}
 	
-	public void applyStates()
+	public void finalize()
 	{
-		int prev_state = this.start_state;
+		int prev_state = this.startState;
 		int prev_slot = 0;
-		for (int item : this.input_states.keySet())
+		for (int item : this.inputStates.keySet())
 		{
-			this.samples[item] = this.input_states.get(item);
+			this.samples[item] = this.inputStates.get(item);
 			// fill previous states
 			for (int i=prev_slot;i<item;i++)
 			{
 				this.samples[i] = prev_state;
 			}
 			// set the prev_state and prev_slot
-			prev_state = this.input_states.get(item);
+			prev_state = this.inputStates.get(item);
 			prev_slot = item + 1;
 		}
 		
