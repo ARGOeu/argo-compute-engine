da9e3ee2ccb81f04d6dfee2725e1345c6d98cf05
Konstantinos Kagkelidis <kaggis@gmail.com>
2015-01-23 10:41:30 +0200

    Add States to Discrete Timeline support

--- status-computation/java/src/main/java/ops/DTimeline.java
+++ status-computation/java/src/main/java/ops/DTimeline.java
@@ -1,12 +1,15 @@
 package ops;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.TreeMap;
 
 public class DTimeline {
 	
-	private static SimpleDateFormat w3c_date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+	
 	
 	private int start_state;	// state to define the beginning of the timeline  
 	private TreeMap<Integer,Integer> input_states; // input states with the timestamp converted to slots
@@ -18,10 +21,11 @@ public class DTimeline {
 	int[] samples;				// array of samples based on sampling frequency		
 	
 	DTimeline()	{
-	
+		start_state = -1;
 		s_period = 1440;			// 1 day = 24 hours = 24 * 60 minutes = 1440 minutes
 		s_interval = 5;				// every 5 minutes;
 		samples = new int[1440/5]; //288 samples;
+		input_states = new TreeMap<Integer,Integer>();
 		Arrays.fill(samples, -1);
 	}
 	
@@ -38,11 +42,31 @@ public class DTimeline {
 	
 	
 	public void clearTimestamps(){
-		
+		start_state = -1;
+		input_states.clear();
 	}
 	
-	public void insertTimestamps(){
+	public void firstState(int _state)
+	{
+		this.start_state = _state;
+	}
+	
+	
+	public int timestampToSlot(String _timestamp) throws ParseException{
+	
+		SimpleDateFormat w3c_date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+		Date parsedDate = w3c_date.parse(_timestamp);
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(parsedDate);
 		
+		int total_minutes = (cal.get(Calendar.HOUR_OF_DAY) * 60) + cal.get(Calendar.MINUTE);
+		
+		return  total_minutes/this.s_interval;
+	}
+	
+	public void insertState(String _timestamp, int _state ) throws ParseException{
+		int slot = this.timestampToSlot(_timestamp);
+		this.input_states.put(slot,_state);
 	}
 	
 
