3c8e2583c6b7592e6d5b749f5bfb16e3ccc3ad44
Konstantinos Kagkelidis <kaggis@gmail.com>
2015-01-28 12:21:41 +0200

    Add HDFS user support. Fix directory hierachies

--- status-computation/lib/avro/upload-sync-data.sh
+++ status-computation/lib/avro/upload-sync-data.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 set -x
+
+# Check the customer argument
+if [ -z "$1" ]
+  then
+    echo "No Customer Argument supplied"
+    exit
+fi
+
+# Check the Job argument
+if [ -z "$2" ]
+  then
+    echo "No Job Argument supplied"
+    exit
+fi
+
+# Check the customer argument
+if [ -z "$3" ]
+  then
+    echo "No Date Argument supplied"
+    exit
+fi
+
 # Gather two arguments given: tenant and date
 TENANT=$1
 JOB=$2
@@ -8,22 +30,26 @@ DAY_UNDER=`echo $DAY_TARGET | sed 's/-/_/g'`
 # Data Locations
 SYNC_DIR=/var/lib/ar-sync
 # Gather Sync Data Filenames
-FN_WEIGHTS=weights_sync_$DAY_UNDER
-FN_DOWNTIMES=downtimes_$DAY_TARGET
-FN_GP_ENDPOINTS=group_endpoints_$DAY_UNDER
-FN_GP_GROUPS=group_groups_$DAY_UNDER
-FN_PROFILE=poem_sync_$DAY_UNDER
+FN_WEIGHTS=weights_sync_$DAY_UNDER.avro
+FN_DOWNTIMES=downtimes_$DAY_TARGET.avro
+FN_GP_ENDPOINTS=group_endpoints_$DAY_UNDER.avro
+FN_GP_GROUPS=group_groups_$DAY_UNDER.avro
+FN_PROFILE=poem_sync_$DAY_UNDER.avro
 # Profile Dir
-PROFILE_DIR=$SYNC_DIR/$TENANT$/$JOB
+PROFILE_DIR=$SYNC_DIR/$TENANT/$JOB
 
 # Create Hadoop Data Directory if not
-hadoop fs -mkdir -p /user/root/$TENANT/sync/$DAY_TARGET
-# Remove previous metric data file 
-hadoop fs -rm -f /user/root/$TENANT/sync/$DAY_TARGET/*
+hadoop fs -mkdir -p /user/$(whoami)/$TENANT/sync/$JOB/
+# Remove previous sync files 
+hadoop fs -rm -f /user/$(whoami)/$TENANT/sync/$JOB/$FN_WEIGHTS
+hadoop fs -rm -f /user/$(whoami)/$TENANT/sync/$JOB/$FN_DOWNTIMES
+hadoop fs -rm -f /user/$(whoami)/$TENANT/sync/$JOB/$FN_GP_ENDPOINTS
+hadoop fs -rm -f /user/$(whoami)/$TENANT/sync/$JOB/$FN_GP_GROUPS
+hadoop fs -rm -f /user/$(whoami)/$TENANT/sync/$JOB/$FN_PROFILE
 # Upload new files
-hadoop fs -put $CONSUMER_DIR/$FN_WEIGHTS /user/root/$TENANT/sync/$DAY_TARGET/
-hadoop fs -put $CONSUMER_DIR/$FN_DOWNTIMES /user/root/$TENANT/sync/$DAY_TARGET/
-hadoop fs -put $CONSUMER_DIR/$FN_GP_ENDPOINTS /user/root/$TENANT/sync/$DAY_TARGET/
-hadoop fs -put $CONSUMER_DIR/$FN_GP_GROUPS /user/root/$TENANT/sync/$DAY_TARGET/
-hadoop fs -put $PROFILE_DIR/$FN_PROFILE /user/root/$TENANT/sync/$DAY_TARGET/
+hadoop fs -put $SYNC_DIR/$FN_WEIGHTS /user/$(whoami)/$TENANT/sync/$JOB/
+hadoop fs -put $SYNC_DIR/$FN_DOWNTIMES /user/$(whoami)/$TENANT/sync/$JOB/
+hadoop fs -put $SYNC_DIR/$FN_GP_ENDPOINTS /user/$(whoami)/$TENANT/sync/$JOB/
+hadoop fs -put $SYNC_DIR/$FN_GP_GROUPS /user/$(whoami)/$TENANT/sync/$JOB/
+hadoop fs -put $PROFILE_DIR/$FN_PROFILE /user/$(whoami)/$TENANT/sync/$JOB/
 
