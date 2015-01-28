a2f7ab11e3b3479f094ddcc7c29e5de086a90682
Konstantinos Kagkelidis <kaggis@gmail.com>
2015-01-28 09:50:34 +0200

    Add support for HDFS user

--- status-computation/lib/avro/upload-consumer-data.sh
+++ status-computation/lib/avro/upload-consumer-data.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 set -x
+# Check the customer argument
+if [ -z "$1" ]
+  then
+    echo "No Customer Argument supplied"
+    exit
+fi
+
+# Check the date argument
+if [ -z "$2" ]
+  then
+    echo "No Date Argument supplied"
+    exit
+fi
+
 # Gather two arguments given: tenant and date
 TENANT=$1
 DAY_TARGET=$2
@@ -9,10 +23,10 @@ CONSUMER_DIR=/var/lib/ar-consumer
 # Gather Metric Data Filename
 FN_CONSUMER=ar-consumer_log_$DAY_TARGET.avro
 # Create Hadoop Data Directory if not
-hadoop fs -mkdir -p /user/root/$TENANT/mdata/
+hadoop fs -mkdir -p /user/$(whoami)/$TENANT/mdata/
 # Remove previous metric data file 
-hadoop fs -rm -f /user/root/$TENANT/mdata/$FN_CONSUMER
+hadoop fs -rm -f /user/$(whoami)/$TENANT/mdata/$FN_CONSUMER
 # Upload new one
-hadoop fs -put $CONSUMER_DIR/$FN_CONSUMER /user/root/$TENANT/mdata/
+hadoop fs -put $CONSUMER_DIR/$FN_CONSUMER /user/$(whoami)/$TENANT/mdata/
 
 
