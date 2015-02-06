bb47ab8cfce62e2fd0330c98906b736b4de33daf
Konstantinos Kagkelidis <kaggis@gmail.com>
2015-02-06 07:32:43 +0200

    Add endpoint group info UDF

---
+++ status-computation/java/src/main/java/ar/AddGroupInfo.java
@@ -0,0 +1,101 @@
+package ar;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.DataType;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.impl.logicalLayer.schema.Schema;
+
+import sync.EndpointGroups;
+import sync.GroupsOfGroups;
+
+public class AddGroupInfo extends EvalFunc<Tuple>{
+	
+	public String fnEndpointGroups;
+	public String fnGroupGroups;
+	
+	public String type;
+	
+	
+	public EndpointGroups endpointMgr;
+	//public GroupsOfGroups groupMgr;
+	
+	private String fsUsed;  // local,hdfs,cache (distrubuted_cache)
+	
+	private boolean initialized;
+
+	public AddGroupInfo(String fnEndpointGroups, String fnGroupGroups, String type, String fsUsed)
+	{
+		this.fnEndpointGroups = fnEndpointGroups;
+		this.fnGroupGroups = fnGroupGroups;
+		
+		this.fsUsed = fsUsed;
+		
+		this.type = type; //type of group
+		
+		this.endpointMgr = new EndpointGroups();
+		//this.groupMgr = new GroupsOfGroups();
+		
+		
+		
+	}
+	
+	
+	public void init() throws IOException
+	{
+		if (this.fsUsed.equalsIgnoreCase("cache")){
+			this.endpointMgr.loadAvro(new File("./endpoint_groups"));
+			//this.groupMgr.loadAvro(new File("./group_groups"));
+		}
+		
+		this.initialized=true;
+	}
+	
+	public List<String> getCacheFiles() { 
+        List<String> list = new ArrayList<String>(); 
+        list.add(this.fnEndpointGroups.concat("#endpoint_groups"));
+        //list.add(this.fnGroupGroups.concat("#group_groups"));
+        return list; 
+	}
+	
+	@Override
+	public Tuple exec(Tuple input) throws IOException {
+		
+		// Check if cache files have been opened 
+		if (this.initialized==false)
+        {
+        	this.init(); // If not open them 
+        }
+		
+		if (input == null || input.size() == 0) return null;
+
+		String service = (String) input.get(0);
+		String hostname = (String) input.get(1);
+		
+		input.append(endpointMgr.getGroup(this.type,hostname,service));
+		
+		return input;
+		
+		
+	}
+	
+	@Override
+    public Schema outputSchema(Schema input) {
+        
+		
+		Schema.FieldSchema group = new Schema.FieldSchema("group", DataType.CHARARRAY);
+		
+		input.add(group);
+
+        return input;
+        
+        
+    }
+	
+	
+	
+}
