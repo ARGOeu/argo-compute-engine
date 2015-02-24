d733f48e0fceb04a008dce00cb48c07c7b87511b
Konstantinos Kagkelidis <kaggis@gmail.com>
2015-02-24 02:13:58 +0200

    Refactor Status Detail UDF

--- status-computation/java/src/main/java/status/PrepStatusDetails.java
+++ status-computation/java/src/main/java/status/PrepStatusDetails.java
@@ -15,37 +15,56 @@ import java.util.logging.Logger;
 import ops.ConfigManager;
 
 import org.apache.pig.EvalFunc;
+import org.apache.pig.data.DataBag;
 import org.apache.pig.data.DataType;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.data.DefaultDataBag;
+import org.apache.pig.data.TupleFactory;
 import org.apache.pig.impl.logicalLayer.FrontendException;
 import org.apache.pig.impl.logicalLayer.schema.Schema;
 
 import sync.AvailabilityProfiles;
 import sync.EndpointGroups;
+import sync.GroupsOfGroups;
 
 
 public class PrepStatusDetails extends EvalFunc<Tuple> {
 	
+	private String fnGgrp;
 	private String fnEgrp;
 	private String fnAps;
 	private String fnCfg;
+	
+	private String targetDate;
 	
 	private String fsUsed;
 
 	public AvailabilityProfiles apsMgr;
 	public EndpointGroups egrpMgr;
+	public GroupsOfGroups ggrpMgr;
 	public ConfigManager cfgMgr;
+	
+	private TupleFactory tupFactory;
 	
 	private boolean initialized;
 	
-	public PrepStatusDetails (String fnEgrp, String fnAps, String fnCfg, String fsUsed){
+	public PrepStatusDetails (String fnGgrp, String fnEgrp, String fnAps, String fnCfg, String targetDate, String fsUsed){
 		
+		this.targetDate = targetDate;
+		
+		this.fnGgrp = fnGgrp;
 		this.fnEgrp= fnEgrp;
 		this.fnAps = fnAps;
 		this.fnCfg = fnCfg;
 		this.fsUsed = fsUsed;
+		
+		this.apsMgr = new AvailabilityProfiles();
+		this.egrpMgr = new EndpointGroups();
+		this.ggrpMgr = new GroupsOfGroups();
+		this.cfgMgr = new ConfigManager();
 	
+		this.tupFactory= TupleFactory.getInstance();
+		
 		this.initialized = false;
 		
 		
@@ -55,14 +74,16 @@ public class PrepStatusDetails extends EvalFunc<Tuple> {
 	public void init() throws IOException
 	{
 		if (this.fsUsed.equalsIgnoreCase("cache")){
-			this.egrpMgr.loadAvro(new File("./endpoint_groups"));
+			this.egrpMgr.loadAvro(new File("./egrp"));
 			this.apsMgr.loadJson(new File("./aps"));
-			this.cfgMgr.loadJson(new File("./endpoint_groups"));
+			this.cfgMgr.loadJson(new File("./cfg"));
+			this.ggrpMgr.loadAvro(new File("./ggrp"));
 		}
 		else if (this.fsUsed.equalsIgnoreCase("local")) {
 			this.egrpMgr.loadAvro(new File(this.fnEgrp));
 			this.apsMgr.loadJson(new File(this.fnAps));
 			this.cfgMgr.loadJson(new File(this.fnCfg));
+			this.ggrpMgr.loadAvro(new File(this.fnGgrp));
 		}
 		
 		this.initialized=true;
@@ -70,9 +91,11 @@ public class PrepStatusDetails extends EvalFunc<Tuple> {
 	
 	public List<String> getCacheFiles() { 
         List<String> list = new ArrayList<String>(); 
-        list.add(this.fnEgrp.concat("#endpoint_groups"));
+        list.add(this.fnGgrp.concat("#ggrp"));
+        list.add(this.fnEgrp.concat("#egrp"));
         list.add(this.fnAps.concat("#aps"));
         list.add(this.fnCfg.concat("#cfg"));
+        
         
         return list; 
 	} 
@@ -80,25 +103,40 @@ public class PrepStatusDetails extends EvalFunc<Tuple> {
 	
 	@Override
 	public Tuple exec(Tuple input) throws IOException {
-			
-		// parse input
-		//String vo = (String)input.get(0);
-		//String vo_fqan = (String)input.get(1);
-		//String monitoring_box = (String)input.get(2);
-		//String roc = (String)input.get(3);
-		//String service_type = (String)input.get(4);
-		String hostname = (String)input.get(5);
-		//String metric = (String)input.get(6);
 		
-		DefaultDataBag timeline = (DefaultDataBag)input.get(7);
+		// Check if cache files have been opened
+		if (this.initialized == false) {
+			this.init(); // If not open them
+		}
+
+		if (input == null || input.size() == 0)
+			return null;
+		
+		// parse input
+		String monitoring_host = (String)input.get(0);
+		String service = (String)input.get(1);
+		String hostname = (String)input.get(2);
+		String metric = (String)input.get(3);
+		
+		DefaultDataBag timeline = (DefaultDataBag)input.get(4);
 		Iterator<Tuple> myit = timeline.iterator();
 		Tuple item;
 		
-		String prevState = "UNKNOWN";
-		String prevTs = "";
+		Tuple output = this.tupFactory.newTuple();
+		DefaultDataBag outBag = new DefaultDataBag();
+		
+		String prevState = "MISSING";
+		String prevTs = this.targetDate.concat("T00:00:00Z");
+		
+		
 		for (int i=0;i<timeline.size();i++)
 		{
 			 item = myit.next();
+			 
+			 String curTsDate = (String)item.get(0);
+			 String curTsDay = curTsDate.substring(0, curTsDate.indexOf("T"));
+			 
+			 
 			 item.append(prevState);
 			 item.append(prevTs);
 			 
@@ -116,30 +154,61 @@ public class PrepStatusDetails extends EvalFunc<Tuple> {
 			 	    
 			 int date_int = (cal.get(Calendar.YEAR) * 10000 ) + ((cal.get(Calendar.MONTH) + 1)*100) + (cal.get(Calendar.DAY_OF_MONTH));
 			 int time_int = (cal.get(Calendar.HOUR_OF_DAY) * 10000) + ((cal.get(Calendar.MINUTE)*100)) + (cal.get(Calendar.SECOND));
-			    
+			 
+			 
 			 item.append(date_int);
 			 item.append(time_int);
 			 
 			 prevState = (String) item.get(1);
 			 prevTs = (String) item.get(0);
+			 
+			 if (curTsDay.equals(this.targetDate))
+			 {
+				 outBag.add(item);
+			 }
 		}
+			 
+		// Find egroup and gggroup
 		
-		return input;
+		String egroupType = this.cfgMgr.egroup;
+		String egroupName = this.egrpMgr.getGroup(egroupType, hostname, service);
+		String ggroupType = this.cfgMgr.ggroup;
+		String ggroupName = this.ggrpMgr.getGroup(ggroupType, egroupName);
+
+		//add stuff to the output
+		
+		output.append(monitoring_host);
+		output.append(service);
+		output.append(hostname);
+		output.append(metric);
+		output.append(ggroupName);
+		output.append(egroupName);
+		output.append(this.cfgMgr.agroup);
+		output.append(this.cfgMgr.agroup);
+		output.append(outBag);
+		
+		return output;
 		
 	}
 	
 	@Override
     public Schema outputSchema(Schema input) {
         
-		Schema.FieldSchema vo = new Schema.FieldSchema("vo", DataType.CHARARRAY);
-		Schema.FieldSchema vo_fqan = new Schema.FieldSchema("vo_fqan", DataType.CHARARRAY);
-		Schema.FieldSchema monitoring_box = new Schema.FieldSchema("monitoring_box", DataType.CHARARRAY);
-		Schema.FieldSchema roc = new Schema.FieldSchema("roc", DataType.CHARARRAY);
+		Schema.FieldSchema monitoring_box = new Schema.FieldSchema("monitoring_host", DataType.CHARARRAY);
 		Schema.FieldSchema hostname = new Schema.FieldSchema("hostname", DataType.CHARARRAY);
-		Schema.FieldSchema service_type = new Schema.FieldSchema("service_type", DataType.CHARARRAY);
+		Schema.FieldSchema service_type = new Schema.FieldSchema("service", DataType.CHARARRAY);
 		Schema.FieldSchema metric = new Schema.FieldSchema("metric", DataType.CHARARRAY);
-		Schema.FieldSchema site = new Schema.FieldSchema("site", DataType.CHARARRAY);
-        
+		Schema.FieldSchema ggroup = new Schema.FieldSchema("ggroup",DataType.CHARARRAY);
+		Schema.FieldSchema egroup = new Schema.FieldSchema("egroup",DataType.CHARARRAY);
+		Schema.FieldSchema altgroup = new Schema.FieldSchema("altg",DataType.CHARARRAY);
+		Schema.FieldSchema altgroupf = new Schema.FieldSchema("altgf",DataType.CHARARRAY);
+		
+		
+		//Schema.FieldSchema egroup = new Schema.FieldSchema("site",DataType.CHARARRAY);
+		//Schema.FieldSchema ggroup = new Schema.FieldSchema("roc",DataType.CHARARRAY);
+		
+		//Schema.FieldSchema altgroup = new Schema.FieldSchema("vo",DataType.CHARARRAY);
+		//Schema.FieldSchema altgroupf = new Schema.FieldSchema("vo_f",DataType.CHARARRAY);
       
         
         
@@ -155,13 +224,16 @@ public class PrepStatusDetails extends EvalFunc<Tuple> {
         Schema status_metric = new Schema();
         Schema timeline = new Schema();
         
-        status_metric.add(vo);
-        status_metric.add(vo_fqan);
+     
         status_metric.add(monitoring_box);
-        status_metric.add(roc);
-        status_metric.add(hostname);
         status_metric.add(service_type);
+        status_metric.add(hostname);
         status_metric.add(metric);
+        status_metric.add(egroup);
+        status_metric.add(ggroup);
+        
+        status_metric.add(altgroup);
+        status_metric.add(altgroupf);
         
         timeline.add(timestamp);
         timeline.add(status);
@@ -180,7 +252,7 @@ public class PrepStatusDetails extends EvalFunc<Tuple> {
         }
         
         status_metric.add(timelines);
-        status_metric.add(site);
+       
         
         try {
             return new Schema(new Schema.FieldSchema("status_metric", status_metric, DataType.TUPLE));
