6faaac20bae1db0039f5c30bcc4f83081d748526
Konstantinos Kagkelidis <kaggis@gmail.com>
2015-03-17 10:39:15 +0200

    Add selection for file or syslog logging

--- standalone/argologger.py
+++ standalone/argologger.py
@@ -3,33 +3,41 @@
 import logging 
 import logging.handlers
 
-def prepare_logger(log_file,log_level,log_name):
+def init_logger(log_mode,log_file,log_level,log_name):
 	# Instantiate logger with proper name
 	logger = logging.getLogger(log_name)
 	logger.setLevel(logging.DEBUG)
-	# Define two handlers (file / console)
-	#file_log = logging.FileHandler(log_file)
-	cmd_log = logging.StreamHandler()
-	sys_log = logging.handlers.SysLogHandler("/dev/log")
-	# The log level for console is always up to info
-	cmd_log.setLevel(logging.INFO)
 
-	# The log level for file is parametized
-	if log_level == 'DEBUG' :
-		sys_log.setLevel(logging.DEBUG)
-	elif log_level == 'INFO':
-		sys_log.setLevel(logging.INFO)
-	elif log_level == 'WARNING':
-		sys_log.setLevel(logging.WARNING)
-	elif log_level == 'ERROR':
-		sys_log.setLevel(logging.ERROR)
-	elif log_level == 'CRITICAL':
-		sys_log.setLevel(logging.CRITICAL)
-	# Define the format for the file log formatting
-	sys_format = logging.Formatter('%(name)s %(levelname)s %(message)s')
-	sys_log.setFormatter(sys_format)
-	# Add handlers 
-	logger.addHandler(sys_log)
+	# Instantiate default levels
+	levels = { 
+		'DEBUG':logging.DEBUG,
+		'INFO':logging.INFO,
+		'WARNING':logging.WARNING,
+		'ERROR':logging.ERROR,
+		'CRITICAL':logging.CRITICAL
+	}	
+
+	# Logger should always have a stream handler to display messages to console
+	cmd_log = logging.StreamHandler()
+	cmd_log.setLevel(logging.INFO)
 	logger.addHandler(cmd_log)
 
+	# If log_mode = file then setup a file handler
+	if log_mode == 'file':
+		
+		file_log = logging.FileHandler(log_file)
+		file_format = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
+		file_log.setFormatter(file_format)
+		
+		logger.addHandler(file_log)
+
+	# If log_mode = syslog then setup a syslog handler 
+	elif log_mode == 'syslog':
+		
+		sys_log = logging.handlers.SysLogHandler("/dev/log")
+		sys_format = logging.Formatter('%(name)s %(levelname)s %(message)s')
+		sys_log.setFormatter(sys_format)
+		
+		logger.addHandler(sys_log)
+	
 	return logger
