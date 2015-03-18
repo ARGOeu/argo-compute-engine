0fc9501e7051f45a7b8a2a976ecbcb1d3a8a2cfa
Konstantinos Kagkelidis <kaggis@gmail.com>
2015-03-18 10:54:29 +0200

    Fix directory path

--- standalone/sync_backup.py
+++ standalone/sync_backup.py
@@ -19,8 +19,8 @@ def main(args=None):
 	arsync_lib = "/var/lib/ar-sync/"
 	arcomp_conf = "/etc/ar-compute/"
 	arcomp_exec = "/usr/libexec/ar-compute/"
-	stdl_exec = "/usr/libexec/ar-compute/standalone"
-	pig_script_path = "/ufsr/libexec/ar-compute/pig/"
+	stdl_exec = "/usr/libexec/ar-compute/standalone/"
+	pig_script_path = "/usr/libexec/ar-compute/pig/"
 
 	ArConfig = SafeConfigParser()
 	ArConfig.read(fn_ar_cfg)
