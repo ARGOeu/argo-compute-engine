d85f14d0d9f1bb53613e099b13be2169dd0c3fa6
Paschalis Korosoglou <pkoro@grid.auth.gr>
2015-04-14 19:40:19 +0300

    ARGO-61 job_ar.py

--- bin/job_ar.py
+++ bin/job_ar.py
@@ -15,8 +15,6 @@ def main(args=None):
 
     # default config
     fn_ar_cfg = "/etc/ar-compute-engine.conf"
-    arsync_exec = "/usr/libexec/ar-sync/"
-    arsync_lib = "/var/lib/ar-sync/"
     arcomp_conf = "/etc/ar-compute/"
     arcomp_exec = "/usr/libexec/ar-compute/"
     stdl_exec = "/usr/libexec/ar-compute/bin"
@@ -31,6 +29,10 @@ def main(args=None):
 
     ArConfig = SafeConfigParser()
     ArConfig.read(fn_ar_cfg)
+
+    # Get sync exec and path
+    arsync_exec = ArConfig.get('connectors', 'sync_exec')
+    arsync_lib = ArConfig.get('connectors', 'sync_path')
 
     # Initialize logging
     log_mode = ArConfig.get('logging', 'log_mode')
