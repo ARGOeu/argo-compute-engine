7e5df7761b1d339fcd680dfef10b9425f5ad9ca8
Konstantinos Kagkelidis <kaggis@gmail.com>
2015-05-06 11:47:18 +0300

    Fix Identation ARGO-48

--- status-computation/java/src/main/java/ops/ConfigManager.java
+++ status-computation/java/src/main/java/ops/ConfigManager.java
@@ -19,19 +19,17 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonParser;
 
-
-
 public class ConfigManager {
 
-	
-	private static final Logger LOG = Logger.getLogger(ConfigManager.class.getName());
-	
+	private static final Logger LOG = Logger.getLogger(ConfigManager.class
+			.getName());
+
 	public String tenant;
 	public String job;
 	public String egroup; // endpoint group
 	public String ggroup; // group of groups
-	public String agroup; // alternative group   
-	public String weight; // weight factor type 
+	public String agroup; // alternative group
+	public String weight; // weight factor type
 	public TreeMap<String, String> egroupTags;
 	public TreeMap<String, String> ggroupTags;
 	public TreeMap<String, String> mdataTags;
@@ -60,69 +58,69 @@ public class ConfigManager {
 		this.mdataTags.clear();
 		this.datastore_map.clear();
 	}
-	
-	public String getMapped(String category, String value)
-	{
-		if (this.datastore_map.containsKey(category))
-		{
+
+	public String getMapped(String category, String value) {
+		if (this.datastore_map.containsKey(category)) {
 			return this.datastore_map.get(category).get(value);
 		}
-		
+
 		return null;
 	}
 
-	public void loadJson(File jsonFile) throws IOException  {
+	public void loadJson(File jsonFile) throws IOException {
 		// Clear data
 		this.clear();
 
-		BufferedReader br=null;
+		BufferedReader br = null;
 		try {
 			br = new BufferedReader(new FileReader(jsonFile));
-		
-		JsonParser jsonParser = new JsonParser();
-		JsonElement jElement = jsonParser.parse(br);
-		JsonObject jObj = jElement.getAsJsonObject();
 
-		// Get the simple fields
-		this.tenant = jObj.getAsJsonPrimitive("tenant").getAsString();
-		this.job = jObj.getAsJsonPrimitive("job").getAsString();
-		this.egroup = jObj.getAsJsonPrimitive("egroup").getAsString();
-		this.ggroup = jObj.getAsJsonPrimitive("ggroup").getAsString();
-		this.weight = jObj.getAsJsonPrimitive("weight").getAsString();
-		this.agroup = jObj.getAsJsonPrimitive("altg").getAsString();
-		// Get compound fields
-		JsonObject jEgroupTags = jObj.getAsJsonObject("egroup_tags");
-		JsonObject jGgroupTags = jObj.getAsJsonObject("ggroup_tags");
-		JsonObject jMdataTags = jObj.getAsJsonObject("mdata_tags");
-		JsonObject jDataMap = jObj.getAsJsonObject("datastore_maps");
-		
-		// Get compound fields
-		for (Entry<String,JsonElement> item : jEgroupTags.entrySet()) {
-			
-			this.egroupTags.put(item.getKey(),item.getValue().getAsString());
-		}
-		for (Entry<String,JsonElement> item : jGgroupTags.entrySet()) {
-			
-			this.ggroupTags.put(item.getKey(),item.getValue().getAsString());
-		}
-		for (Entry<String,JsonElement> item : jMdataTags.entrySet()) {
-			
-			this.mdataTags.put(item.getKey(),item.getValue().getAsString());
-		}
-		
-		
-		// Get super compound field
-		for (Entry<String,JsonElement> item : jDataMap.entrySet()) {
-			String itemKey = item.getKey();
-			this.datastore_map.put(itemKey,new HashMap<String,String>());
-			
-			JsonObject jSubObj = item.getValue().getAsJsonObject();
-			for (Entry<String,JsonElement> subitem : jSubObj.entrySet())
-			{
-				this.datastore_map.get(itemKey).put(subitem.getKey(), subitem.getValue().getAsString());
+			JsonParser jsonParser = new JsonParser();
+			JsonElement jElement = jsonParser.parse(br);
+			JsonObject jObj = jElement.getAsJsonObject();
+
+			// Get the simple fields
+			this.tenant = jObj.getAsJsonPrimitive("tenant").getAsString();
+			this.job = jObj.getAsJsonPrimitive("job").getAsString();
+			this.egroup = jObj.getAsJsonPrimitive("egroup").getAsString();
+			this.ggroup = jObj.getAsJsonPrimitive("ggroup").getAsString();
+			this.weight = jObj.getAsJsonPrimitive("weight").getAsString();
+			this.agroup = jObj.getAsJsonPrimitive("altg").getAsString();
+			// Get compound fields
+			JsonObject jEgroupTags = jObj.getAsJsonObject("egroup_tags");
+			JsonObject jGgroupTags = jObj.getAsJsonObject("ggroup_tags");
+			JsonObject jMdataTags = jObj.getAsJsonObject("mdata_tags");
+			JsonObject jDataMap = jObj.getAsJsonObject("datastore_maps");
+
+			// Get compound fields
+			for (Entry<String, JsonElement> item : jEgroupTags.entrySet()) {
+
+				this.egroupTags.put(item.getKey(), item.getValue()
+						.getAsString());
 			}
-		}
-		
+			for (Entry<String, JsonElement> item : jGgroupTags.entrySet()) {
+
+				this.ggroupTags.put(item.getKey(), item.getValue()
+						.getAsString());
+			}
+			for (Entry<String, JsonElement> item : jMdataTags.entrySet()) {
+
+				this.mdataTags
+						.put(item.getKey(), item.getValue().getAsString());
+			}
+
+			// Get super compound field
+			for (Entry<String, JsonElement> item : jDataMap.entrySet()) {
+				String itemKey = item.getKey();
+				this.datastore_map.put(itemKey, new HashMap<String, String>());
+
+				JsonObject jSubObj = item.getValue().getAsJsonObject();
+				for (Entry<String, JsonElement> subitem : jSubObj.entrySet()) {
+					this.datastore_map.get(itemKey).put(subitem.getKey(),
+							subitem.getValue().getAsString());
+				}
+			}
+
 		} catch (FileNotFoundException ex) {
 			LOG.error("Could not open file:" + jsonFile.getName());
 			throw ex;
@@ -134,8 +132,6 @@ public class ConfigManager {
 			// Close quietly without exceptions the buffered reader
 			IOUtils.closeQuietly(br);
 		}
-		
-		
 
 	}
 
