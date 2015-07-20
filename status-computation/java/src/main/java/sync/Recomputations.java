0d068ba366f2b9fb160630eeb0bf12f77240a7d2
Konstantinos Kagkelidis <kaggis@gmail.com>
2015-07-20 10:40:27 +0300

    ARGO-190 Identation fix

--- status-computation/java/src/main/java/sync/Recomputations.java
+++ status-computation/java/src/main/java/sync/Recomputations.java
@@ -22,155 +22,147 @@ import com.google.gson.JsonParser;
 
 public class Recomputations {
 
-	private static final Logger LOG = Logger.getLogger(Recomputations.class.getName());
+    private static final Logger LOG = Logger.getLogger(Recomputations.class.getName());
 
-	public ArrayList<RecompItem> list;
+    public ArrayList<RecompItem> list;
 
-	private class RecompItem {
-		String reason; // Recalculation reason
-		String status; // Recalculation status
-		String supergroup; // Name of supergroup that adheres to recalculation
-		String start; // Start timestamp
-		String end; // Start timestamp
-		String submitted;
-		ArrayList<String> exclude; // Exclude list
+    private class RecompItem {
+        String reason; // Recalculation reason
+        String status; // Recalculation status
+        String supergroup; // Name of supergroup that adheres to recalculation
+        String start; // Start timestamp
+        String end; // Start timestamp
+        String submitted;
+        ArrayList<String> exclude; // Exclude list
 
-		public RecompItem() {
-			// Initializations
-			this.reason = "";
-			this.status = "";
-			this.supergroup = "";
-			this.start = "";
-			this.end = "";
-			this.submitted = "";
-			this.exclude = new ArrayList<String>();
-		}
+        public RecompItem() {
+            // Initializations
+            this.reason = "";
+            this.status = "";
+            this.supergroup = "";
+            this.start = "";
+            this.end = "";
+            this.submitted = "";
+            this.exclude = new ArrayList<String>();
+        }
 
-		public RecompItem(String reason, String status, String supergroup,
-				String start, String end, String submitted,
-				ArrayList<String> exclude) {
-			this.reason = reason;
-			this.status = status;
-			this.supergroup = supergroup;
-			this.start = start;
-			this.end = end;
-			this.submitted = submitted;
-			this.exclude = exclude;
-		}
-	}
+        public RecompItem(String reason, String status, String supergroup, String start, String end, String submitted,
+                ArrayList<String> exclude) {
+            this.reason = reason;
+            this.status = status;
+            this.supergroup = supergroup;
+            this.start = start;
+            this.end = end;
+            this.submitted = submitted;
+            this.exclude = exclude;
+        }
+    }
 
-	public Recomputations() {
-		this.list = new ArrayList<RecompItem>();
-	}
+    public Recomputations() {
+        this.list = new ArrayList<RecompItem>();
+    }
 
-	// Clear all the recalc data
-	public void clear() {
-		this.list = new ArrayList<RecompItem>();
-	}
+    // Clear all the recalc data
+    public void clear() {
+        this.list = new ArrayList<RecompItem>();
+    }
 
-	public void insert(String reason, String status, String supergroup,
-			String start, String end, String submitted,
-			ArrayList<String> exclude) {
-		this.list.add(new RecompItem(reason, status, supergroup, start, end,
-				submitted, exclude));
-	}
+    public void insert(String reason, String status, String supergroup, String start, String end, String submitted,
+            ArrayList<String> exclude) {
+        this.list.add(new RecompItem(reason, status, supergroup, start, end, submitted, exclude));
+    }
 
-	public int count() {
-		return this.list.size();
-	}
+    public int count() {
+        return this.list.size();
+    }
 
-	public boolean shouldRecompute(String supergroup, String groupname, String targetDate)
-			throws ParseException {
+    public boolean shouldRecompute(String supergroup, String groupname, String targetDate) throws ParseException {
 
-		for (RecompItem item : this.list) {
-			// supergroup found
-			if (item.supergroup.equalsIgnoreCase(supergroup)) {
+        for (RecompItem item : this.list) {
+            // supergroup found
+            if (item.supergroup.equalsIgnoreCase(supergroup)) {
 
-				// loop through all excluded sites
-				for (String subitem : item.exclude) {
-				    // if site exists in the exclude list
-					if (groupname.equalsIgnoreCase(subitem)) {
-					    
-					    // check if the dates alingn
+                // loop through all excluded sites
+                for (String subitem : item.exclude) {
+                    // if site exists in the exclude list
+                    if (groupname.equalsIgnoreCase(subitem)) {
+
+                        // check if the dates alingn
                         SimpleDateFormat dmy = new SimpleDateFormat("yyyy-MM-dd");
                         Date sDate = dmy.parse(item.start);
                         Date eDate = dmy.parse(item.end);
                         Date tDate = dmy.parse(targetDate);
-                        
-                        return (tDate.compareTo(sDate) >= 0 && tDate.compareTo(eDate) <= 0); 
-					    
-					}
-				}
-			}
-		}
 
-		// Site doesn't belong in recomputation exclude list
-		return false;
-	}
+                        return (tDate.compareTo(sDate) >= 0 && tDate.compareTo(eDate) <= 0);
 
-	public String getStart(String supergroup) {
-		for (RecompItem item : this.list) {
-			if (item.supergroup.equalsIgnoreCase(supergroup)) {
-				return item.start;
-			}
-		}
+                    }
+                }
+            }
+        }
 
-		return null;
-	}
+        // Site doesn't belong in recomputation exclude list
+        return false;
+    }
 
-	public String getEnd(String supergroup) {
-		for (RecompItem item : this.list) {
-			if (item.supergroup.equalsIgnoreCase(supergroup)) {
-				return item.end;
-			}
-		}
+    public String getStart(String supergroup) {
+        for (RecompItem item : this.list) {
+            if (item.supergroup.equalsIgnoreCase(supergroup)) {
+                return item.start;
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	public void loadJson(File jsonFile) throws IOException {
+    public String getEnd(String supergroup) {
+        for (RecompItem item : this.list) {
+            if (item.supergroup.equalsIgnoreCase(supergroup)) {
+                return item.end;
+            }
+        }
 
-		this.clear();
+        return null;
+    }
 
-		BufferedReader br = null;
-		try {
-			br = new BufferedReader(new FileReader(jsonFile));
+    public void loadJson(File jsonFile) throws IOException {
 
-			JsonParser jsonParser = new JsonParser();
-			JsonElement jRootElement = jsonParser.parse(br);
-			JsonArray jRootObj = jRootElement.getAsJsonArray();
+        this.clear();
 
-			for (JsonElement item : jRootObj) {
-				String reason = item.getAsJsonObject().get("r").getAsString();
-				String status = item.getAsJsonObject().get("s").getAsString();
-				String start = item.getAsJsonObject().get("st").getAsString();
-				String end = item.getAsJsonObject().get("et").getAsString();
-				String submitted = item.getAsJsonObject().get("t")
-						.getAsString();
-				String supergroup = item.getAsJsonObject().get("n")
-						.getAsString();
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(jsonFile));
 
-				ArrayList<String> exclude = new ArrayList<String>();
-				// Get the excluded
-				JsonArray jExclude = item.getAsJsonObject().get("es")
-						.getAsJsonArray();
-				for (JsonElement subitem : jExclude) {
-					exclude.add(subitem.getAsString());
-				}
+            JsonParser jsonParser = new JsonParser();
+            JsonElement jRootElement = jsonParser.parse(br);
+            JsonArray jRootObj = jRootElement.getAsJsonArray();
 
-				this.insert(reason, status, supergroup, start, end, submitted,
-						exclude);
-			}
+            for (JsonElement item : jRootObj) {
+                String reason = item.getAsJsonObject().get("r").getAsString();
+                String status = item.getAsJsonObject().get("s").getAsString();
+                String start = item.getAsJsonObject().get("st").getAsString();
+                String end = item.getAsJsonObject().get("et").getAsString();
+                String submitted = item.getAsJsonObject().get("t").getAsString();
+                String supergroup = item.getAsJsonObject().get("n").getAsString();
 
-		} catch (FileNotFoundException ex) {
-			LOG.error("Could not open file:" + jsonFile.getName());
-			throw ex;
+                ArrayList<String> exclude = new ArrayList<String>();
+                // Get the excluded
+                JsonArray jExclude = item.getAsJsonObject().get("es").getAsJsonArray();
+                for (JsonElement subitem : jExclude) {
+                    exclude.add(subitem.getAsString());
+                }
 
-		} finally {
-			// Close quietly without exceptions the buffered reader
-			IOUtils.closeQuietly(br);
-		}
+                this.insert(reason, status, supergroup, start, end, submitted, exclude);
+            }
 
-	}
+        } catch (FileNotFoundException ex) {
+            LOG.error("Could not open file:" + jsonFile.getName());
+            throw ex;
+
+        } finally {
+            // Close quietly without exceptions the buffered reader
+            IOUtils.closeQuietly(br);
+        }
+
+    }
 
 }
--- status-computation/java/src/test/java/sync/RecomputationsTest.java
+++ status-computation/java/src/test/java/sync/RecomputationsTest.java
@@ -16,38 +16,38 @@ import org.junit.Test;
 
 public class RecomputationsTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		// Assert that files are present
-		assertNotNull("Test file missing", RecomputationsTest.class.getResource("/ops/recomp.json"));
-	}
-	
-	@Test
-	public void test() throws URISyntaxException, ParseException, IOException {
-		//Prepare Resource File
-		URL resJsonFile = RecomputationsTest.class.getResource("/ops/recomp.json");
-		File jsonFile = new File(resJsonFile.toURI());
-		
-		Recomputations recMgr = new Recomputations();
-		recMgr.loadJson(jsonFile);
-		
-		assertEquals(recMgr.shouldRecompute("NGI_GRNET", "GR-01-AUTH", "2013-12-09"),true);
-		assertEquals(recMgr.shouldRecompute("NGI_GRNET", "GR-01-AUTH", "2013-12-10"),true);
-		assertEquals(recMgr.shouldRecompute("NGI_GRNET", "GR-01-AUTH", "2013-12-08"),true);
-		// should return false because date is out of recomputation period
-		assertEquals(recMgr.shouldRecompute("NGI_GRNET", "GR-01-AUTH", "2013-12-07"),false);
-        assertEquals(recMgr.shouldRecompute("NGI_GRNET", "GR-01-AUTH", "2013-12-11"),false);
-        assertEquals(recMgr.shouldRecompute("NGI_GRNET", "GR-01-AUTH", "2013-08-02"),false);
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        // Assert that files are present
+        assertNotNull("Test file missing", RecomputationsTest.class.getResource("/ops/recomp.json"));
+    }
+
+    @Test
+    public void test() throws URISyntaxException, ParseException, IOException {
+        // Prepare Resource File
+        URL resJsonFile = RecomputationsTest.class.getResource("/ops/recomp.json");
+        File jsonFile = new File(resJsonFile.toURI());
+
+        Recomputations recMgr = new Recomputations();
+        recMgr.loadJson(jsonFile);
+
+        assertEquals(recMgr.shouldRecompute("NGI_GRNET", "GR-01-AUTH", "2013-12-09"), true);
+        assertEquals(recMgr.shouldRecompute("NGI_GRNET", "GR-01-AUTH", "2013-12-10"), true);
+        assertEquals(recMgr.shouldRecompute("NGI_GRNET", "GR-01-AUTH", "2013-12-08"), true);
+        // should return false because date is out of recomputation period
+        assertEquals(recMgr.shouldRecompute("NGI_GRNET", "GR-01-AUTH", "2013-12-07"), false);
+        assertEquals(recMgr.shouldRecompute("NGI_GRNET", "GR-01-AUTH", "2013-12-11"), false);
+        assertEquals(recMgr.shouldRecompute("NGI_GRNET", "GR-01-AUTH", "2013-08-02"), false);
         // should return false because site is out of exclude list
-        assertEquals(recMgr.shouldRecompute("NGI_GRNET", "GR-04-IASA", "2013-12-09"),false);
-        assertEquals(recMgr.shouldRecompute("NGI_GRNET", "GR-04-IASA", "2013-12-10"),false);
-        assertEquals(recMgr.shouldRecompute("NGI_GRNET", "GR-04-IASA", "2013-12-08"),false);
-        // should return false because NGI doesn't belong in the recomputation request 
-        assertEquals(recMgr.shouldRecompute("NGI_UK", "SITEA", "2013-12-09"),false);
-        assertEquals(recMgr.shouldRecompute("NGI_DE", "SITEB", "2013-12-10"),false);
-        assertEquals(recMgr.shouldRecompute("NGI_FRANCE", "SITEC", "2013-12-08"),false);
-        
-		
-	}
+        assertEquals(recMgr.shouldRecompute("NGI_GRNET", "GR-04-IASA", "2013-12-09"), false);
+        assertEquals(recMgr.shouldRecompute("NGI_GRNET", "GR-04-IASA", "2013-12-10"), false);
+        assertEquals(recMgr.shouldRecompute("NGI_GRNET", "GR-04-IASA", "2013-12-08"), false);
+        // should return false because NGI doesn't belong in the recomputation
+        // request
+        assertEquals(recMgr.shouldRecompute("NGI_UK", "SITEA", "2013-12-09"), false);
+        assertEquals(recMgr.shouldRecompute("NGI_DE", "SITEB", "2013-12-10"), false);
+        assertEquals(recMgr.shouldRecompute("NGI_FRANCE", "SITEC", "2013-12-08"), false);
+
+    }
 
 }
